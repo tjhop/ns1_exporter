6a54fbf33fa582ab933b8b17695f85e2ee61d6bf
TJ Hoplock <t.hoplock@gmail.com>
2024-01-05 20:42:55 -0500

    test: add internal ns1/core api tests

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/prometheus/client_golang v1.17.0
 	github.com/prometheus/common v0.45.0
 	github.com/prometheus/exporter-toolkit v0.10.0
+	github.com/stretchr/testify v1.8.2
 	gopkg.in/ns1/ns1-go.v2 v2.7.13
 )
 
@@ -17,12 +18,14 @@ require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/coreos/go-systemd/v22 v22.5.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-logfmt/logfmt v0.5.1 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/jpillora/backoff v1.0.0 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/matttproud/golang_protobuf_extensions/v2 v2.0.0 // indirect
 	github.com/mwitkow/go-conntrack v0.0.0-20190716064945-2f068394615f // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_model v0.4.1-0.20230718164431-9a2bf3000d16 // indirect
 	github.com/prometheus/procfs v0.11.1 // indirect
 	github.com/xhit/go-str2duration/v2 v2.1.0 // indirect
@@ -35,4 +38,5 @@ require (
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ pkg/ns1/api_test.go
@@ -0,0 +1,108 @@
+// Copyright 2023 TJ Hoplock
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ns1
+
+import (
+	"fmt"
+	"net/url"
+	"regexp"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"gopkg.in/ns1/ns1-go.v2/mockns1"
+	api "gopkg.in/ns1/ns1-go.v2/rest"
+	"gopkg.in/ns1/ns1-go.v2/rest/model/dns"
+)
+
+func TestRefreshZoneData(t *testing.T) {
+	mock, doer, err := mockns1.New(t)
+	require.Nil(t, err)
+	defer mock.Shutdown()
+
+	mockClient := api.NewClient(doer, api.SetAPIKey("mockAPIKey"))
+	mockClient.Endpoint, err = url.Parse(fmt.Sprintf("https://%s/v1/", mock.Address))
+	require.NoError(t, err)
+
+	tests := map[string]struct {
+		zoneEnabled   bool
+		recordEnabled bool
+		zoneBlacklist *regexp.Regexp
+		zoneWhitelist *regexp.Regexp
+		want          map[string]*Zone
+		expectedLen   int
+	}{
+		"recordsDisabled": {recordEnabled: false, zoneEnabled: false, zoneBlacklist: nil, zoneWhitelist: nil, want: map[string]*Zone{
+			"foo.bar": {},
+			"keep.me": {},
+			"drop.me": {},
+		}, expectedLen: 3},
+		"recordsEnabled": {recordEnabled: true, zoneEnabled: false, zoneBlacklist: nil, zoneWhitelist: nil, want: map[string]*Zone{
+			"foo.bar": {Zone: "foo.bar", Records: []*ZoneRecord{{Domain: "test", ShortAns: []string{"dead::beef"}, Type: "AAAA"}}},
+			"keep.me": {Zone: "keep.me", Records: []*ZoneRecord{{Domain: "test", ShortAns: []string{"1.2.3.4"}, Type: "A"}}},
+			"drop.me": {Zone: "drop.me", Records: []*ZoneRecord{{Domain: "test", ShortAns: []string{"5.6.7.8"}, Type: "A"}}},
+		}, expectedLen: 3},
+		"blacklist": {recordEnabled: true, zoneEnabled: false, zoneBlacklist: regexp.MustCompile("drop.+"), zoneWhitelist: nil, want: map[string]*Zone{
+			"foo.bar": {Zone: "foo.bar", Records: []*ZoneRecord{{Domain: "test", ShortAns: []string{"dead::beef"}, Type: "AAAA"}}},
+			"keep.me": {Zone: "keep.me", Records: []*ZoneRecord{{Domain: "test", ShortAns: []string{"1.2.3.4"}, Type: "A"}}},
+		}, expectedLen: 2},
+		"whitelist": {recordEnabled: true, zoneEnabled: false, zoneBlacklist: nil, zoneWhitelist: regexp.MustCompile("keep.+"), want: map[string]*Zone{
+			"keep.me": {Zone: "keep.me", Records: []*ZoneRecord{{Domain: "test", ShortAns: []string{"1.2.3.4"}, Type: "A"}}},
+		}, expectedLen: 1},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			require.Nil(t, mock.AddZoneListTestCase(nil, nil,
+				[]*dns.Zone{
+					{Zone: "foo.bar", Records: []*dns.ZoneRecord{{Domain: "test", ShortAns: []string{"dead::beef"}, Type: "AAAA"}}},
+					{Zone: "keep.me", Records: []*dns.ZoneRecord{{Domain: "test", ShortAns: []string{"1.2.3.4"}, Type: "A"}}},
+					{Zone: "drop.me", Records: []*dns.ZoneRecord{{Domain: "test", ShortAns: []string{"5.6.7.8"}, Type: "A"}}},
+				},
+			))
+
+			getRecords := tc.recordEnabled || tc.zoneEnabled
+
+			require.Nil(t, mock.AddZoneGetTestCase("foo.bar", nil, nil,
+				&dns.Zone{Zone: "foo.bar", Records: []*dns.ZoneRecord{{Domain: "test", ShortAns: []string{"dead::beef"}, Type: "AAAA"}}},
+				getRecords,
+			))
+
+			require.Nil(t, mock.AddZoneGetTestCase("keep.me", nil, nil,
+				&dns.Zone{Zone: "keep.me", Records: []*dns.ZoneRecord{{Domain: "test", ShortAns: []string{"1.2.3.4"}, Type: "A"}}},
+				getRecords,
+			))
+
+			require.Nil(t, mock.AddZoneGetTestCase("drop.me", nil, nil,
+				&dns.Zone{Zone: "drop.me", Records: []*dns.ZoneRecord{{Domain: "test", ShortAns: []string{"5.6.7.8"}, Type: "A"}}},
+				getRecords,
+			))
+
+			got := RefreshZoneData(mockClient, getRecords, tc.zoneBlacklist, tc.zoneWhitelist)
+			require.Equal(t, tc.want, got)
+			require.Equal(t, tc.expectedLen, len(got))
+			for _, zone := range got {
+				switch getRecords {
+				case true:
+					require.Equal(t, 1, len(zone.Records))
+				default:
+					require.Equal(t, 0, len(zone.Records))
+				}
+			}
+
+			// clear test cases for next iteration
+			mock.ClearTestCases()
+		})
+	}
+}
